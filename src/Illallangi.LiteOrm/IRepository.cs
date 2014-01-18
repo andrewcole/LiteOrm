20f0e2d201c9e4feee6c0bb73ec5a37431ed9030
Andrew Cole <andrew.cole@illallangi.com>
2014-01-18 21:14:24 +1100

    Added IRepository and RepositoryBase

---
+++ src/Illallangi.LiteOrm/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Illallangi.LiteOrm
+{
+    using System.Collections.Generic;
+
+    public interface IRepository<T> where T : class
+    {
+        T Create(T obj);
+
+        IEnumerable<T> Retrieve(T obj = null);
+
+        T Update(T obj);
+
+        void Delete(T obj);
+    }
+}
---
+++ src/Illallangi.LiteOrm/RepositoryBase.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Data.SQLite;
+using Common.Logging;
+    
+namespace Illallangi.LiteOrm
+{
+    public abstract class RepositoryBase<T> : IRepository<T> where T : class
+    {
+        #region Fields
+
+        private readonly IConnectionSource currentConnectionSource;
+
+        private readonly ILog currentLog;
+
+        #endregion
+
+        #region Constructors
+
+        protected RepositoryBase(IConnectionSource connectionSource, ILog log)
+        {
+            this.currentConnectionSource = connectionSource;
+            this.currentLog = log;
+            this.Log.DebugFormat(
+                @"RepositoryBase(connectionSource=""{0}"", log=""{1}"")",
+                this.ConnectionSource,
+                this.Log);
+        }
+
+        #endregion
+
+        #region Properties
+
+        #region Protected Properties
+
+        protected ILog Log
+        {
+            get { return this.currentLog; }
+        }
+
+        #endregion
+
+        #region Private Properties
+
+        private IConnectionSource ConnectionSource
+        {
+            get { return this.currentConnectionSource; }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Methods
+
+        public abstract T Create(T obj);
+
+        public abstract IEnumerable<T> Retrieve(T obj = null);
+
+        public abstract T Update(T obj);
+
+        public abstract void Delete(T obj);
+
+        protected SQLiteConnection GetConnection()
+        {
+            return this.ConnectionSource.GetConnection();
+        }
+
+        #endregion
+    }
+}
